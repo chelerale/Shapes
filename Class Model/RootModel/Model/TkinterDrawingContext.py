5e8046b037e147006181ab6c0e2aaa777959e736
Andrew Naumovich <andrew.naumovich@yandex.ru>
2019-03-05 13:06:00 +0300

    Paramter controller

--- Class Model/RootModel/Model/TkinterDrawingContext.py
+++ Class Model/RootModel/Model/TkinterDrawingContext.py
@@ -2,23 +2,42 @@ from RootModel.Model.DrawingContext import DrawingContext
 from typing import *
 from tkinter import Canvas
 
+def parameter_count_controller(count_param: int, err_msg: str):
+    def decorator(func : Callable):
+        def wrapper(*args, **kwargs):
+            if (len(args) != count_param):
+                raise ValueError(
+                    "Invalid parameter count in function {}, params got: {}, tip : {}".format(
+                        func, args, err_msg
+                    )
+                )
+            result = func(*args, **kwargs)
+            return result
+        return wrapper
+    return decorator
+
+
 class TkinterDrawingContext(DrawingContext):
 
     def __init__(self, canvas: Canvas) -> None:
         if canvas is None:
             raise ValueError('None canvas provided')
 
-        self.__canvas = canvas
+        self.__canvas : Canvas = canvas
         super().__init__()
 
     def draw_arc(params : Tuple[float, ...]):
         raise NotImplementedError('Implement me')
 
+    @parameter_count_controller(4, 'Top-left and bottom-right expected')
     def draw_ellipse(params : Tuple[float, ...]):
-        raise NotImplementedError('Implement me')
+        x0, y0, x1, y0 = *params
+        self.__canvas.create_oval(x0, y0, x1, y1)
 
-    def draw_point(Point : Tuple[float, ...]):
-        raise NotImplementedError('Implement me')
+    @parameter_count_controller(2, '(x, y) exprected')
+    def draw_point(params : Tuple[float, ...]):
+        x, y, r = *params, 1
+        self.__canvas.create_oval(x-r, y-r, x+r, y+r, width=2)
 
     def draw_rectangle(Point : Tuple[float, ...]):
         raise NotImplementedError('Implement me')
